1be8113ea246ac828352cccbff75cb1ac527d3dc
AVB9907 <aleidavaldivieso@gmail.com>
2026-04-15 09:54:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -341,7 +341,7 @@ else:
             col1, col2, col3, col4, col5 = st.columns(5)
 
             with col1:
-                if st.button("Gestión de vehículos", use_container_width=True):
+                if st.button("https://img.icons8.com/?size=100&id=7493&format=png&color=000000", use_container_width=True):
                     st.session_state.pagina = "vehiculos"
                     st.session_state.subpagina = "menu"
                     st.rerun()
